ac27efdf881f639c88a26f80e91e416f780d05a5
luisrdevy <luisrdevy480@gmail.com>
2021-06-07 20:53:41 -0500

    example

--- example.js
+++ example.js
@@ -11,17 +11,15 @@ const port = new SerialPort("COM3", {
 
 port.pipe(parser);
 
-// t30h20d150 temp, humedad, dist
-parser.on("data", (data) => {
-  const ti = data.indexOf("t");
-  const hi = data.indexOf("h");
-  const di = data.indexOf("d");
-  const temp = data.substring(ti + 1, hi);
-  const hume = data.substring(hi + 1, di);
-  const dist = data.substring(di + 1);
+const serverUrl = "localhost:4000";
 
-  console.log("data received: ", data);
-  console.log("temp: ", temp);
-  console.log("humedad: ", hume);
-  console.log("distancia: ", dist);
+// t30h20d150 temp, humedad, dist
+parser.on("data", async (data) => {
+  const res = await fetch(serverUrl, {
+    method: "POST",
+    "Content-Type": "application/json",
+    body: {
+      data,
+    },
+  });
 });
