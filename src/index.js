1d0526cd09b34e7858fd69449cca955cdb54adff
luisrdevy <luisrdevy480@gmail.com>
2021-06-14 15:03:17 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set("port", process.env.PORT || 4000);
 
 // middlewares
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
