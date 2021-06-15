e8657812d66b6c3466929c30542d3f3e88304672
luisrdevy <luisrdevy480@gmail.com>
2021-06-15 09:42:44 -0500

    check 3 parts

--- example.js
+++ example.js
@@ -9,19 +9,39 @@ const port = new SerialPort("COM3", {
   flowControl: false,
 });
 
+port.write("WARMACHINE ON");
+
 port.pipe(parser);
 
 const serverUrl = "https://sensores-instru.herokuapp.com";
 
 // t30h20d150 temp, humedad, dist
-parser.on("data", async (data) => {
-  const res = await fetch(serverUrl, {
-    method: "POST",
-    "Content-Type": "application/json",
-    body: {
-      data,
-    },
+// parser.on("data", async (data) => {
+//   const res = await fetch(serverUrl, {
+//     method: "POST",
+//     "Content-Type": "application/json",
+//     body: {
+//       data,
+//     },
+//   });
+//   const data = await res.json();
+//   console.log(data); // { temperatura, humedad, distancia }
+// });
+
+// parser.on("data", (data) => {
+//   fetch(serverUrl, {
+//     method: "POST",
+//     "Content-Type": "application/json",
+//     body: {
+//       data,
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// });
+
+parser.on("data", (data) => {
+  axios.post(serverUrl, { data }).then((data) => {
+    parser.emit("data", data);
   });
-  const data = res.json();
-  console.log(data);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,28 +12,61 @@ const db = admin.database();
 const TEMP = "temperatura";
 const HUME = "humedad";
 const DIST = "distancia";
+const KEYS = {
+  T: "temperature",
+  TH: "temperatureHistory",
+  H: "humidity",
+  HH: "humidityHistory",
+  D: "distance",
+  DH: "distanceHistory",
+};
 
-const router = Router();
-
-router.get("/", async (req, res) => {
-  const temperatura = (await db.ref(TEMP).once("value")).val();
-  const humedad = (await db.ref(HUME).once("value")).val();
-  const distancia = (await db.ref(DIST).once("value")).val();
-  res.json({ temperatura, humedad, distancia });
-});
-
-router.post("/", (req, res) => {
-  const { data } = req.body;
+const useData = (data) => {
   const ti = data.indexOf("t");
   const hi = data.indexOf("h");
   const di = data.indexOf("d");
   const temperatura = Number(data.substring(ti + 1, hi));
   const humedad = Number(data.substring(hi + 1, di));
   const distancia = Number(data.substring(di + 1));
-  db.ref(TEMP).set(temperatura);
-  db.ref(HUME).set(humedad);
-  db.ref(DIST).set(distancia);
-  res.json({ temperatura, humedad, distancia });
+  return [temperatura, humedad, distancia];
+};
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const temperatura = (await db.ref(KEYS.T).once("value")).val();
+  const humedad = (await db.ref(KEYS.H).once("value")).val();
+  const distancia = (await db.ref(KEYS.D).once("value")).val();
+  const temperaturaHistory = (await db.ref(KEYS.TH).once("value")).val();
+  const humedadHistory = (await db.ref(KEYS.HH).once("value")).val();
+  const distanciaHistory = (await db.ref(KEYS.DH).once("value")).val();
+  res.json({
+    temperatura,
+    temperaturaHistory,
+    humedad,
+    humedadHistory,
+    distancia,
+    distanciaHistory,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const [t, h, d] = useData(req.body.data);
+  db.ref(KEYS.T).set(t);
+  db.ref(KEYS.H).set(h);
+  db.ref(KEYS.D).set(d);
+
+  const th = (await db.ref(KEYS.TH).get()).val();
+  const hh = (await db.ref(KEYS.HH).get()).val();
+  const dh = (await db.ref(KEYS.DH).get()).val();
+
+  if (t != th[th.length - 1]) db.ref(KEYS.TH).set([...th, t]);
+
+  if (h != hh[hh.length - 1]) db.ref(KEYS.HH).set([...hh, h]);
+
+  if (d != dh[dh.length - 1]) db.ref(KEYS.DH).set([...dh, d]);
+
+  res.json({ motor: 0 });
 });
 
 module.exports = router;
